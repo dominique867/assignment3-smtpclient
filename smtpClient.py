a79e8b3d4f29f466d21f6438f3d0855eab01bd38
Dominique Wimmer <dwimmer867@gmail.com>
2026-06-25 01:41:21 -0400

    filled in skeleton code

--- smtpClient.py
+++ smtpClient.py
@@ -10,10 +10,12 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    #print(recv) #You can use these print statement to validate return codes from the server.
+    #print(recv) #You can use these print statements to validate return codes from the server.
     #if recv[:3] != '220':
     #    print('220 reply not received from server.')
 
@@ -27,26 +29,41 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    mailCommand = 'MAIL FROM: <alice@alice.com>\r\n'
+    clientSocket.send(mailCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    rcptCommand = 'RCPT TO: <bob@bob.com>\r\n'
+    clientSocket.send(rcptCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    rec4 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send message data.
     # Fill in start
+    clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    clientSocket.send(endmsg.encode())
+    recv5 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    quitCommand = 'QUIT\r\n'
+    clientSocket.send(quitCommand.encode())
+    recv6 = clientSocket.recv(1024).decode()
     # Fill in end
 
 
